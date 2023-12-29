06deec90ca1fbeaeab2dd7308368d4733982639c
Nikita Filonenko <nikdfil@yandex.ru>
2023-12-29 16:35:09 +0300

    fix web app

--- Book.Notifier/Program.cs
+++ Book.Notifier/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using BookRetail_API.Messages;
+using EasyNetQ;
+using Microsoft.AspNetCore.SignalR.Client;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
-Console.WriteLine("Hello, World!");
+namespace Book.Notifier
+{
+    internal class Program
+    {
+        const string SIGNALR_HUB_URL = "https://localhost:7041/hub";
+        private static HubConnection hub;
+
+        static async Task Main(string[] args)
+        {
+            hub = new HubConnectionBuilder().WithUrl(SIGNALR_HUB_URL).Build();
+            await hub.StartAsync();
+            Console.WriteLine("Hub started!");
+            Console.WriteLine("Press any key to send a message (Ctrl-C to quit)");
+            var amqp = "amqp://user:rabbitmq@localhost:5672";
+            using var bus = RabbitHutch.CreateBus(amqp);
+            Console.WriteLine("Connected to bus! Listening for newBookMessages");
+            var subscriberId = $"Book.Notifier@{Environment.MachineName}";
+            await bus.PubSub.SubscribeAsync<NewBookPriceMessage>(subscriberId, HandleNewVehicleMessage);
+            Console.ReadLine();
+        }
+
+        private static async void HandleNewVehicleMessage(NewBookPriceMessage nvpm)
+        {
+            var csvRow =
+                $"{nvpm.Price} {nvpm.CurrencyCode} : {nvpm.Title},{nvpm.PublisherName}," +
+                $"{nvpm.ModelCode},{nvpm.PublicationYear},{nvpm.Genre},{nvpm.Author},{nvpm.CreatedAt:O}";
+            Console.WriteLine(csvRow);
+            var json = JsonSerializer.Serialize(nvpm, JsonSettings());
+            await hub.SendAsync("NotifyWebUsers", "Book.Notifier",
+                json);
+        }
+
+        static JsonSerializerOptions JsonSettings() =>
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+    }
+}
