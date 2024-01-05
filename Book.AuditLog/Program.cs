170b58964287eba9a2645e189440f1bf1d8fb209
Nikita Filonenko <nikdfil@yandex.ru>
2024-01-05 16:02:34 +0300

    fix

--- Book.AuditLog/Program.cs
+++ Book.AuditLog/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-namespace BookRetail_API
+namespace Book.AuditLog
 {
     public class Program
     {
